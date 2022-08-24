f5958d4132014ff63fb649541b32a85d6713b203
Alan Jaime <alandario9016@gmail.com>
2022-08-23 22:30:14 -0300

    se agrego components navbar/tailwind

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+const NavBar = () => {
+    return(
+        
+          <nav>
+                <div className="logo">
+                    <h1>Mi App</h1>
+                </div>
+                <ul>
+                <li><a href="#">Index</a></li>
+                <li><a href="#">Nosotros</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Contáctanos</a></li>
+                </ul>
+        </nav>
+        
+    );
+}
+export default NavBar;
